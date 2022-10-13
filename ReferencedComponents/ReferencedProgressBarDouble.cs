91203061c554b32bc3208a4a7288f9a5db64e793
Somindras <57025128+AntoninJuquel@users.noreply.github.com>
2022-10-13 13:13:37 +0200

    added gradient

---
+++ ReferencedComponents/ReferencedProgressBarDouble.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ReferenceSharing.ReferencedComponents
+{
+    public class ReferencedProgressBarDouble : ReferencedComponent<double>
+    {
+        [SerializeField] private Gradient gradient;
+        [SerializeField] private Reference<double> maxDoubleValueRef;
+        private Image _fill;
+
+        private void Awake()
+        {
+            _fill = GetComponent<Image>();
+        }
+
+        protected override void OnValueChanged(double value)
+        {
+            _fill.fillAmount = (float)(valueRef.Value / maxDoubleValueRef.Value);
+            _fill.color = gradient.Evaluate(_fill.fillAmount);
+        }
+    }
+}
