4510e8ba4cef4822e21a045960f99faf1df26aa7
AntoninJuquel <antonin.juquel@outlook.fr>
2023-05-04 17:05:52 +0200

    added color

---
+++ Variables/ColorVariable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace ReferenceSharing.Variables
+{
+    [CreateAssetMenu(menuName = "Variables/color")]
+    public class ColorVariable : Variable<Color>
+    {
+    }
+}
