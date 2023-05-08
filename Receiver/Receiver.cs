c3ec889e11754a6be1fa8148459af8923e744c03
AntoninJuquel <antonin.juquel@outlook.fr>
2023-05-08 17:33:02 +0200

    refresh on enable

--- Receiver/Receiver.cs
+++ Receiver/Receiver.cs
@@ -6,11 +6,17 @@ namespace ReferenceSharing
     public class Receiver<T> : MonoBehaviour
     {
         [SerializeField] private Variable<T> variable;
+        [SerializeField] private bool refreshOnEnable = true;
         [SerializeField] private UnityEvent<T> onValueChanged;
 
         private void OnEnable()
         {
             variable.OnValueChanged += RaiseEvent;
+            
+            if (refreshOnEnable)
+            {
+                RaiseEvent(variable.Value);
+            }
         }
 
         private void OnDisable()
